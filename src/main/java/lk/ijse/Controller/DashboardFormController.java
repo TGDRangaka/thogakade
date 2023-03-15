fb1c1695dbff60169a7758fc40c267bae2855288
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2023-03-15 10:35:19 +0530

    views and controllers added

--- src/main/java/lk/ijse/Controller/DashboardFormController.java
+++ src/main/java/lk/ijse/Controller/DashboardFormController.java
@@ -3,7 +3,11 @@ package lk.ijse.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
 
@@ -11,13 +15,17 @@ public class DashboardFormController {
     private AnchorPane root;
 
     @FXML
-    void btnCustomerOnAction(ActionEvent event) {
-        root.setVisible(false);
+    void btnCustomerOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/customer_form"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(scene);
     }
 
     @FXML
     void btnHomeOnAction(ActionEvent event) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/"));
+        root.setVisible(false);
     }
 
     @FXML
